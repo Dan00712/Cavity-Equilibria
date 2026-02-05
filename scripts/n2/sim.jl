25a16d83ce96d853b20ed18535449a70e49721c4
Daniel Steinmaurer <dan.steinmaurer@gmail.com>
2026-02-06 00:12:58 +0100

    redo to use different initial values
    uses max 3 values per \Delta; much mure performant great success

--- scripts/n2/sim.jl
+++ scripts/n2/sim.jl
@@ -1,12 +1,11 @@
-#!/usr/bin/env julia
 using DrWatson
-HOME = ENV["HOME"]
 DrWatson.quickactivate(
-    joinpath(HOME, "projects", "bac", "CavityEquilibria"),
-    "CavityEquilibria",
+    "CavityEquilibria"
 )
 
 using Random
+
+using Artifacts, LazyArtifacts
 
 using JLD2
 using Plots
@@ -21,83 +20,64 @@ using CavityEquilibria.Laser
 using CavityEquilibria.RootFinding
 using CavityEquilibria.Util
 
+
 const params = DEFAULT_PARAMS
-
-const Φ = [0]
-
 const N = 2
-const sz = let
-    VALS = 15
-    Random.seed!(0)
+const κ = 18e4*2π
+const ϕ = 0 
 
-    #range is [-2, 2]
-    logsz = rand(VALS) .* 4 .- 2
-    1e-6 .* vcat(exp10.(logsz), -1 .* exp10.(logsz))
-end
-const vz = Iterators.product([sz for _ = 1:N]...)
-const d = 1.5e-6    # m === 1.5 μm
-const κ = 18e4 * 2π
-const Δ = let
-    # kc * d/2 = 2 π
-    kc = 4π/d
-    # c = ωc/kc
-    ωc = params.c * kc
-    
-    ωc - params.ω0
-end
-const vd = [-d/2, d/2]
-
-
-ϕ, z = let
-    ϕ = []
-    z = []
-    for ϕ_ in ProgressBar(Φ)
-        for zg in vz
-            try
-                r = find_roots(collect(zg), vd, [0, ϕ_]; Δ = Δ, κ = κ, params = params)
-
-                if (length(z) == 0 || !any(v -> isapprox(v, r), z)) && all(abs.(r) .< 1)
-                    push!(z, r)
-                    push!(ϕ, ϕ_)
-                end
-            catch err
-                if !(err isa ConvergenceError)
-                    rethrow(err)
-                end
-            end
-        end
-    end
-
-    z = Iterators.reduce(hcat, z)
-
-    ϕ, z
+M = let
+	@load datadir("P1", "latest.jld2") M
+    M
 end
 
-const PREFIX="n2-single"
-let
-    p = joinpath(datadir(PREFIX) |> mkpath, "latest.jld2")
-    M = (ϕ=ϕ, z = z)
-    @save p M
+Δs = unique(M.Δ)
+function zsp(Δ)
+	M.z[M.Δ .== Δ]
 end
+
+z = []
+ω = []
+φ = []
+	
+for Δ in ProgressBar(Δs)
+	l = params.λ0 * (1- Δ/params.ω0)
+	vϕ = [0, ϕ]
+	vd = [-l/2, l/2]
+	for zg in Iterators.product([zsp(Δ) for _ in 1:N]...)
+		try
+			r = find_roots(
+				collect(zg), vd, vϕ; 
+				Δ = Δ, κ = κ, params = params
+			)
+		
+			if (length(z) == 0 || !any(v-> isapprox(v, r), z) && all(abs.(r) .< 1))
+				push!(z, r)
+				push!(ω, Δ)
+                push!(φ, ϕ)
+			end
+		catch err
+			if !(err isa ConvergenceError)
+				rethrow(err)
+			end
+		end
+	end
+end
+
+z = Iterators.reduce(hcat, z)
 
 p = plot(;
-#    zlabel = "Δ/ 2π kHz",
-    xlabel = "z1/μm",
-    ylabel = "z2/μm",
-    #yscale=:log,
-    #xscale=:log,
+  xaxis=:log,
+  xlims=(10^-2, 50),
+  xlabel="z1/μm",
+  yaxis=:log,
+  ylims=(10^-2, 50),
+  ylabel="z2/μm",
+  zlabel="Δ/ 2πkHz"
 )
-
-scatter!(
-    p,
-    z[1, :] .* 1e6, # x
-    z[2, :] .* 1e6, # y
-#    ϕ;
-#    markersize = 5,
+scatter!(p,
+         z[1, :] .* 1e6, z[2, :] .* 1e6, ω /(2π*1e3)
 )
-savefig(p, joinpath(mkpath(plotsdir(PREFIX)), "latest.html"))
-savefig(p, joinpath(mkpath(plotsdir(PREFIX)), "latest.pdf"))
-savefig(p, joinpath(mkpath(plotsdir(PREFIX)), "$(now_nodots()).png"))
 
 if isinteractive()
     gui(p)
