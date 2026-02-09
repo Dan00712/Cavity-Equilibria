f2afe620439bc23ad4fe354cec3911e25fa25f66
Daniel 'Percy' Steinmaurer <dan.steinmaurer@gmail.com>
2026-02-09 14:55:00 +0100

    add single phi sim 3d plot output with interpolation

---
+++ scripts/n2/splot-3d.jl
@@ -0,0 +1,58 @@
+using DrWatson
+DrWatson.quickactivate(
+    "CavityEquilibria"
+)
+
+using Logging
+
+using JLD2
+using Plots
+plotlyjs()
+default(show= false)
+using ProgressBars
+
+using ScatteredInterpolation
+
+using CavityEquilibria.Util
+
+const PREFIX = "n2"
+const N = 2
+const φ = 0
+
+ω, ϕ, z = let
+	@load datadir(PREFIX, "latest.jld2") M
+
+    selector = M.z[1,:] .> 0 .&& M.z[2, :] .> 0
+    M.Δ[selector], M.ϕ[selector], M.z[:, selector]
+end
+
+itp = interpolate(Polyharmonic(2), z, ω)
+z1_ = exp.(range(log.(extrema(z[1, :]))..., 100))
+z2_ = exp.(range(log.(extrema(z[2, :]))..., 100))
+Ds = [evaluate(itp, [z1i, z2i])[1] for z1i in z1_, z2i in z2_]
+
+p = plot(;
+    xaxis=:log,
+    xlims=(10^-2, 50),
+    xlabel="z1/μm",
+    yaxis=:log,
+    ylims=(10^-2, 50),
+    ylabel="z2/μm",
+    zlabel="Δ/ 2πkHz"
+)
+scatter!(p,
+         z[1, :] .* 1e6, z[2, :] .* 1e6, ω /(2π*1e3),
+         label="equilibria-positions"
+)
+surface!(p, z1_ .* 1e6, z2_ .* 1e6, Ds ./(2π*1e3),
+       label="interpolated surface",
+       colorbar=false
+      )
+
+  
+savefig(p, joinpath(mkpath(plotsdir(PREFIX)), "latest.html"))
+savefig(p, joinpath(mkpath(plotsdir(PREFIX)), "$(now_nodots()).html"))
+
+if isinteractive()
+    gui(p)
+end
